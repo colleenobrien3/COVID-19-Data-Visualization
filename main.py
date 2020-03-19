238f1f23859f79be371b5347ef84f95b886bf334
Colleen_O'Brien <colleen3117@gmail.com>
2020-03-18 22:51:00 -0400

    web scraping apparently doesn't work in virtual environment

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ from datetime import datetime
 import urllib
 from bs4 import BeautifulSoup
 
-quote_page = ''
+quote_page = 'https://forecast.weather.gov/MapClick.php?x=161&y=208&site=phi&zmx=&zmy=&map_x=161&map_y=208#.XnKvo5NKjGI'
 
 page = urllib.request.urlopen(quote_page)
 
 soup = BeautifulSoup(page, 'html.parser')
 
-name_box = soup.find_all('div', class_='')
+name_box = soup.find(id='seven-day-forecast-list')
 
 # # strip() is used to remove starting and trailing
 # name = name_box.innerText().strip()
