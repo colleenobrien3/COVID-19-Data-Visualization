fcd35cd834ce555ee52cc0c37deb852dacda5fc4
Colleen_O'Brien <colleen3117@gmail.com>
2020-03-20 09:43:09 -0400

    got web scraping data better understood

--- main.py
+++ main.py
@@ -9,18 +9,27 @@ from datetime import datetime
 import urllib
 from bs4 import BeautifulSoup
 
+# quote_page = 'https://www.worldometers.info/coronavirus/country/us/'
+
+# page = urllib.request.urlopen(quote_page)
+
+# soup = BeautifulSoup(page, 'html.parser')
+
+# name_box = soup.find_all('td')
+
+# # # strip() is used to remove starting and trailing
+# # name = name_box.innerText().strip()
+
+# for i in name_box:
+#     print(''.join(i.get_text().strip()))
+
+
+# ------------------------
+
 quote_page = 'https://www.worldometers.info/coronavirus/country/us/'
-
 page = urllib.request.urlopen(quote_page)
-
 soup = BeautifulSoup(page, 'html.parser')
-
 name_box = soup.find_all('td')
-
-# # strip() is used to remove starting and trailing
-# name = name_box.innerText().strip()
 
 for i in name_box:
     print(''.join(i.get_text().strip()))
-
-# print(soup.get_text())
