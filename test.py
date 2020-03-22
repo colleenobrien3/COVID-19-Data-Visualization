356ea76d386b4112d6f7787a5958e42a6bb3ad36
Colleen_O'Brien <colleen3117@gmail.com>
2020-03-22 17:56:33 -0400

    didn't save

--- test.py
+++ test.py
@@ -6,3 +6,13 @@ db = PostgresqlDatabase('states', user='postgres', password='',
 db.connect()
 
 print(State.select())
+
+with
+user_plays as (
+    select
+    user_id, count(1) as num_plays
+    from
+    gameplays
+    group by
+    1
+)
